2d83aab848941d82468fd44df111bffeed1112ec
Samiya Kazi <samiya.kazi09@gmail.com>
2023-12-20 10:18:08 +0600

    feat: added base server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import cors from 'cors';
+import express, { Express, Request, Response } from 'express';
+import config from './config';
+
+const app: Express = express();
+
+app.use(
+  cors({
+    origin: config.CORS_ORIGIN.split(','),
+  })
+);
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello! I am working!</h1>');
+});
+
+app.listen(config.PORT, () => {
+  console.log(`[server]: Server is running on port ${config.PORT}`);
+});
