7e4fa7355a6daf59d04029a020a6ea1153382651
Alfred Pithu <alfredpithu@gmail.com>
2024-01-09 12:38:17 +0600

    changed data structure

--- server/src/routers/serviceAuth.router.ts
+++ server/src/routers/serviceAuth.router.ts
@@ -13,6 +13,6 @@ serviceAuthRouter.get("/redirect/:service", verifyJWTMiddleware, redirectToServi
 // From Silo-backend to Skeleton
 serviceAuthRouter.get("/token/:code", getTokenFromStore);
 
-serviceAuthRouter.get("user-from-token", verifyJWTMiddleware, getUserInfoByToken);
+serviceAuthRouter.get("/user-from-token", verifyJWTMiddleware, getUserInfoByToken);
 
 export default serviceAuthRouter;
