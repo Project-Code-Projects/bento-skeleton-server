0b3289701e4d05e2793da66c41a5b022db83d2aa
Alfred Pithu <alfredpithu@gmail.com>
2024-01-13 22:58:05 +0600

    cors utility added

--- server/src/utilities/cors.utility.ts
+++ server/src/utilities/cors.utility.ts
@@ -1,7 +1,8 @@
 import config from "../config";
 
-export function getCorsOrigin () {
+export function getCorsOrigin() {
   return [
+    "http://localhost:4200",
     config.HR_BE_BASE_URL,
     config.INVENTORY_BE_BASE_URL,
     config.KDS_BE_BASE_URL,
@@ -9,6 +10,6 @@ export function getCorsOrigin () {
     config.MENU_BE_BASE_URL,
     config.POS_BE_BASE_URL,
     config.REVIEW_BE_BASE_URL,
-    ...config.CORS_ORIGIN.split(',')
-  ]
+    ...config.CORS_ORIGIN.split(","),
+  ];
 }
