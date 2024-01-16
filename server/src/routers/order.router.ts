d6a0cbc4f998d1d1da98248a8349f0572ffc0733
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-16 19:07:11 +0600

    fix: added missing router url

--- server/src/routers/order.router.ts
+++ server/src/routers/order.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import verifyJWTMiddleware from "../middlewares/verifyJWT.middleware";
-import { getAllOrders } from "../controllers/order.controller";
+import { getAllOrders, updateOrderStatus } from "../controllers/order.controller";
 const orderRouter = Router();
 
 orderRouter.get('/all', verifyJWTMiddleware, getAllOrders);
+orderRouter.put('/status', verifyJWTMiddleware, updateOrderStatus);
 
 export default orderRouter;
