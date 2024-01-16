b5bc426ff28d5ce3b6018b491c4275e242355061
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-16 17:01:37 +0600

    fex: added missing controller

--- server/src/routers/order.router.ts
+++ server/src/routers/order.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import verifyJWTMiddleware from "../middlewares/verifyJWT.middleware";
+import { getAllOrders } from "../controllers/order.controller";
 const orderRouter = Router();
 
-orderRouter.get('/all', verifyJWTMiddleware);
+orderRouter.get('/all', verifyJWTMiddleware, getAllOrders);
 
 export default orderRouter;
