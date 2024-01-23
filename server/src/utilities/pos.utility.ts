eb65e64021ce6183ee14dc29dd50a86ddccbb25b
Alfred Pithu <alfredpithu@gmail.com>
2024-01-23 13:52:24 +0600

    env file updated

--- server/src/utilities/pos.utility.ts
+++ server/src/utilities/pos.utility.ts
@@ -94,7 +94,9 @@ export async function sendOrderIdWithFullOrderToKdsFromPosToMarkOrderAsServed(or
 // Get hourly/weekday/monthly stats from POS
 export async function getStatsFromPos(timespan: string, token: string) {
   try {
+    console.log("timespanInParams", timespan);
     const res = await axios.get<any>(`${config.POS_BE_BASE_URL}/order/stats/${timespan}`, { headers: { 'Authorization': 'Bearer ' + token } })
+    console.log("res.data", res.data);
     return res.data
   } catch (error) {
     console.log(error);
