0025222151bb4cf581c1f7310a1ff4bbccda3938
Alfred Pithu <alfredpithu@gmail.com>
2024-01-25 16:33:56 +0600

    changed waiter efficiency api route

--- server/src/utilities/hr.utility.ts
+++ server/src/utilities/hr.utility.ts
@@ -118,7 +118,7 @@ export async function hrPostChefEfficiency(data: any, token: string) {
 // Post waiter efficiency to HR
 export async function hrPostWaiterEfficiency(data: any, token: string) {
   try {
-    const res = await axios.post(`${config.HR_BE_BASE_URL}/waiter-efficiency`, data, { headers: { 'Authorization': 'Bearer ' + token } })
+    const res = await axios.post(`${config.HR_BE_BASE_URL}/waiter/efficiency`, data, { headers: { 'Authorization': 'Bearer ' + token } })
     return res.data
   } catch (error) {
     console.log(error);
