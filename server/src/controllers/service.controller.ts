a308d74e2fefd3e7e02ea3c7895acab9605334ce
Alfred Pithu <alfredpithu@gmail.com>
2024-01-28 21:55:56 +0600

    data added for employee 0

--- server/src/controllers/service.controller.ts
+++ server/src/controllers/service.controller.ts
@@ -47,7 +47,45 @@ export async function getUserInfoByToken(req: JwtReqInterface, res: Response) {
   try {
     const user = req.user;
     if (!user) return res.send({ message: 'Unauthorized.' });
-
+    if (user.id === 0) {
+      return res.send({
+        "user": {
+          "positionId": 34,
+          "employeeInformation": {
+            "id": 0,
+            "restaurantId": 0,
+            "name": "super admin",
+            "email": "admin@gmail.com",
+            "experience": [],
+            "phoneNumber": 1624752189,
+            "address": "Tongi",
+            "skillTags": [],
+            "hourlyRate": 25,
+            "efficiency": "",
+            "imageUrl": "",
+            "createdAt": "2024-01-24T09:02:40.687Z",
+            "updatedAt": "2024-01-24T09:13:14.571Z",
+            "applicantId": null,
+            "position": {
+              "id": 34,
+              "position": "Manager",
+              "employeeId": 32,
+              "restaurantId": 1,
+              "services": [
+                "INVENTORY",
+                "MENU-BUILDER",
+                "KDS",
+                "POS",
+                "MARKETPLACE",
+                "HR"
+              ],
+              "createdAt": "2024-01-24T09:03:00.902Z",
+              "updatedAt": "2024-01-24T09:13:14.401Z"
+            }
+          }
+        }
+      })
+    }
     const userData = await hrUserInfo(user.id);
     res.send(userData);
   } catch (error) {
