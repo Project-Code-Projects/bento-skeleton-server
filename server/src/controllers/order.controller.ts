d3181ee2ad965ab5dd919749d747affc8995ba5b
Alfred Pithu <alfredpithu@gmail.com>
2024-02-24 19:41:26 +0600

    Revert "done for the day"
    This reverts commit 82876d04294567b8f7aea308a8ab4bcbac401589.

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -6,8 +6,6 @@ import { IOrder } from "../interfaces/NewOrderInterface";
 import { preparePlusRestructureOrderDataForInventory, sendDataToInventoryToReduce } from "../utilities/processOrder.utility";
 import { getMarketplaceOrderData, marketplaceUpdateOrderStatus } from "../utilities/marketplace.utility";
 
-import amqp, { Channel, Connection } from "amqplib"
-
 // Get All Orders from POS and Marketplace
 export async function getAllOrders(req: JwtReqInterface, res: Response) {
   try {
@@ -95,46 +93,12 @@ export async function updateOrderChef(req: JwtReqInterface, res: Response) {
 
 
 
-// 1. OLD Send New Order To KDS + Inventory To Process Order
-// export async function incomingOrder(req: JwtReqInterface, res: Response) {
-//   try {
-//     const { user } = req;
-
-//     if (!user) return res.status(401).send({ message: 'Unauthorized.' });
-
-//     const order: IOrder = req.body.order;
-
-
-//     let result;
-
-//     if (order.type == "in-house") {
-//       result = await kdsPostIncomingOrder(user.token, order);
-//     }
-
-//     else if (order.type === "pickup" || order.type === "delivery") {
-
-//       result = await kdsPostIncomingOrder(user.token, order);
-//       await console.log('Result from KDS');
-
-//       const restructuredOrderDataForInventory = preparePlusRestructureOrderDataForInventory(order)
-//       if (result) {
-//         let inventoryResult = await sendDataToInventoryToReduce(restructuredOrderDataForInventory, user.token);
-//         return res.send(inventoryResult)
-//       }
-
-//     }
-
-//     res.status(201).send({ message: 'Successfully sent to KDS', data: result });
-
-//   } catch (error) {
-//     // console.log('😭😭😭😭😭😭😭😭😭😭' , error);
-//     res.status(500).send({ message: (error as Error).message });
-//   }
-// }
 
 
 
-// With RABBIT MQ
+
+
+// 1. Send New Order To KDS + Inventory To Process Order
 export async function incomingOrder(req: JwtReqInterface, res: Response) {
   try {
     const { user } = req;
@@ -150,69 +114,24 @@ export async function incomingOrder(req: JwtReqInterface, res: Response) {
       result = await kdsPostIncomingOrder(user.token, order);
     }
 
-    // else if (order.type === "pickup" || order.type === "delivery") {
+    else if (order.type === "pickup" || order.type === "delivery") {
 
-    //   result = await kdsPostIncomingOrder(user.token, order);
-    //   await console.log('Result from KDS');
+      result = await kdsPostIncomingOrder(user.token, order);
+      await console.log('Result from KDS');
 
-    // const restructuredOrderDataForInventory = preparePlusRestructureOrderDataForInventory(order)
-    //   if (result) {
-    //     let inventoryResult = await sendDataToInventoryToReduce(restructuredOrderDataForInventory, user.token);
-    //     return res.send(inventoryResult)
-    //   }
+      const restructuredOrderDataForInventory = preparePlusRestructureOrderDataForInventory(order)
+      if (result) {
+        let inventoryResult = await sendDataToInventoryToReduce(restructuredOrderDataForInventory, user.token);
+        return res.send(inventoryResult)
+      }
 
-    // }
+    }
 
     res.status(201).send({ message: 'Successfully sent to KDS', data: result });
 
   } catch (error) {
     // console.log('😭😭😭😭😭😭😭😭😭😭' , error);
     res.status(500).send({ message: (error as Error).message });
-  }
-}
-
-
-
-
-const queue = "marketplaceOrder"
-let connection: Connection;
-let channel: Channel;
-
-// Connect and Create rabbit mq channel and connection
-export async function connectAndconsumeMQDataForMarketplaceOrders() {
-
-  try {
-    const ampqServer = "amqps://ujuxbuct:HxHHm8XNtbtohKTPHi30fSdILcP9FhGQ@armadillo.rmq.cloudamqp.com/ujuxbuct"
-    connection = await amqp.connect(ampqServer)
-    channel = await connection.createChannel();
-
-    await channel.assertQueue(queue, { durable: false })
-
-    await channel.consume(queue, (data) => {
-      if (data) {
-        console.log('data has come');
-        const order = JSON.parse(data.content.toString())
-        console.log('Order From Queue', order);
-
-      }
-    }, { noAck: true }) // noAck true for now. Make it false when KDS and INVENTORY not giving any more error
-
-  } catch (err) {
-    console.log(err);
-  } finally {
-    // if (channel) await channel.close()
-  }
-
-}
-
-// Close rabbitmq connection and channel
-export async function closeMQConnection() {
-  try {
-    if (connection) await connection.close()
-    if (channel) await channel.close()
-
-  } catch (error) {
-    console.log(error);
   }
 }
 
