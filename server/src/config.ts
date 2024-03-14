100258538adf3569215dff54fa9a9e406e37a8a9
Alfred Pithu <alfredpithu@gmail.com>
2024-03-14 23:04:33 +0600

    readme updated

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,36 +1,75 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+// const config = {
+//   DB_PASS: process.env.DB_PASS,
+//   DB_USER: process.env.DB_USER,
+
+//   PORT: process.env.PORT ?? 5000,
+//   CORS_ORIGIN: process.env.CORS_ORIGIN ?? "*",
+//   JWT_SECRET: process.env.JWT_SECRET ?? "secret",
+
+//   HR_BE_BASE_URL: process.env.HR_BE_BASE_URL,
+//   HR_FE_BASE_URL: process.env.HR_FE_BASE_URL,
+
+//   INVENTORY_BE_BASE_URL: process.env.INVENTORY_BE_BASE_URL,
+//   INVENTORY_FE_BASE_URL: process.env.INVENTORY_FE_BASE_URL,
+
+//   KDS_BE_BASE_URL: process.env.KDS_BE_BASE_URL,
+//   KDS_FE_BASE_URL: process.env.KDS_FE_BASE_URL,
+
+//   POS_BE_BASE_URL: process.env.POS_BE_BASE_URL,
+//   POS_FE_BASE_URL: process.env.POS_FE_BASE_URL,
+
+//   MARKETPLACE_BE_BASE_URL: process.env.MARKETPLACE_BE_BASE_URL,
+//   MARKETPLACE_FE_BASE_URL: process.env.MARKETPLACE_FE_BASE_URL,
+
+//   MENU_BE_BASE_URL: process.env.MENU_BE_BASE_URL,
+//   MENU_FE_BASE_URL: process.env.MENU_FE_BASE_URL,
+
+//   REVIEW_BE_BASE_URL: process.env.REVIEW_BE_BASE_URL,
+//   REVIEW_FE_BASE_URL: process.env.REVIEW_FE_BASE_URL,
+
+//   RESERVATION_FE_BASE_URL: process.env.RESERVATION_FE_BASE_URL,
+
+
+//   REDIS_PORT: process.env.REDIS_PORT,
+//   REDIS_HOST: process.env.REDIS_HOST,
+//   REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+
+// };
+
+
 const config = {
   DB_PASS: process.env.DB_PASS,
   DB_USER: process.env.DB_USER,
 
   PORT: process.env.PORT ?? 5000,
-  CORS_ORIGIN: process.env.CORS_ORIGIN,
-  JWT_SECRET: process.env.JWT_SECRET,
+  CORS_ORIGIN: process.env.CORS_ORIGIN ?? "*",
+  JWT_SECRET: process.env.JWT_SECRET ?? "secret",
 
-  HR_BE_BASE_URL: process.env.HR_BE_BASE_URL,
+  HR_BE_BASE_URL: process.env.HR_BE_BASE_URL ?? "http://localhost:4000",
   HR_FE_BASE_URL: process.env.HR_FE_BASE_URL,
 
-  INVENTORY_BE_BASE_URL: process.env.INVENTORY_BE_BASE_URL,
-  INVENTORY_FE_BASE_URL: process.env.INVENTORY_FE_BASE_URL,
+  INVENTORY_BE_BASE_URL: process.env.INVENTORY_BE_BASE_URL ?? "http://localhost:4010",
+  INVENTORY_FE_BASE_URL: process.env.INVENTORY_FE_BASE_URL ?? "http://localhost:4011",
 
-  KDS_BE_BASE_URL: process.env.KDS_BE_BASE_URL,
-  KDS_FE_BASE_URL: process.env.KDS_FE_BASE_URL,
+  KDS_BE_BASE_URL: process.env.KDS_BE_BASE_URL ?? "http://localhost:4020",
+  KDS_FE_BASE_URL: process.env.KDS_FE_BASE_URL ?? "http://localhost:4021",
 
-  POS_BE_BASE_URL: process.env.POS_BE_BASE_URL,
-  POS_FE_BASE_URL: process.env.POS_FE_BASE_URL,
+  POS_BE_BASE_URL: process.env.POS_BE_BASE_URL ?? "http://localhost:4030",
+  POS_FE_BASE_URL: process.env.POS_FE_BASE_URL ?? "http://localhost:4031",
 
-  MARKETPLACE_BE_BASE_URL: process.env.MARKETPLACE_BE_BASE_URL,
-  MARKETPLACE_FE_BASE_URL: process.env.MARKETPLACE_FE_BASE_URL,
+  MARKETPLACE_BE_BASE_URL: process.env.MARKETPLACE_BE_BASE_URL ?? "http://localhost:4040",
+  MARKETPLACE_FE_BASE_URL: process.env.MARKETPLACE_FE_BASE_URL ?? "http://localhost:4041",
 
-  MENU_BE_BASE_URL: process.env.MENU_BE_BASE_URL,
-  MENU_FE_BASE_URL: process.env.MENU_FE_BASE_URL,
+  MENU_BE_BASE_URL: process.env.MENU_BE_BASE_URL ?? "http://localhost:4050",
+  MENU_FE_BASE_URL: process.env.MENU_FE_BASE_URL ?? "http://localhost:4051",
 
-  REVIEW_BE_BASE_URL: process.env.REVIEW_BE_BASE_URL,
-  REVIEW_FE_BASE_URL: process.env.REVIEW_FE_BASE_URL,
+  REVIEW_BE_BASE_URL: process.env.REVIEW_BE_BASE_URL ?? "http://localhost:4060",
+  REVIEW_FE_BASE_URL: process.env.REVIEW_FE_BASE_URL ?? "http://localhost:4061",
 
-  RESERVATION_FE_BASE_URL: process.env.RESERVATION_FE_BASE_URL,
+  RESERVATION_FE_BASE_URL: process.env.RESERVATION_FE_BASE_URL ?? "http://localhost:4150",
 
 
   REDIS_PORT: process.env.REDIS_PORT,
